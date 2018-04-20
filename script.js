1eaf664c9348891e6544427cc7769602b2a4a987
Xubiod <pitts.davis3@hotmail.com>
2018-04-19 21:51:34 -0400

    heheheh

---
+++ script.js
@@ -0,0 +1,52 @@
+//data from refreshing https://github.com/new an unhealthy amount of times
+//some data was just made up
+
+var firstlevel = [
+    "refactored", "potential",
+    "urban", "sturdy", "literate",
+    "laughing", "supreme", "legendary",
+    "verbose", "curly", "automatic",
+    "upgraded", "bookish", "no", "null",
+    "game", "website", "old", "shiny", "super",
+    "effective", "novice", "css", "html", "csharp",
+    "obj-c", "expert", "c", "rust", "cpp", "node-js",
+    "tcp", "udp", "minecraft", "java", "lua", "excessive",
+    "sad", "happy", "unity", "gamemaker", "interactive", "sleepy",
+    "murderous", "grumpy", "keymash", "random"
+];
+
+var secondlevel = [
+    "potato", "fortnight",
+    "train", "carnival",
+    "happiness", "lamp",
+    "umbrella", "octo",
+    "disco", "robot",
+    "bot", "relay", "mod",
+    "code", "repo", "framework",
+    "library", "api", "website",
+    "memory", "tool", "garbanzo",
+    "management", "giggle", "story",
+    "journey", "royale", "exploit", "guide",
+    "ebook", "tutorial", "plugin", "extension",
+    "archive", "script", "secret", "program", "source"
+];
+
+function generate() {
+    var generated_con = firstlevel[Math.floor(Math.random() * firstlevel.length)] + "-" + secondlevel[Math.floor(Math.random() * secondlevel.length)];
+
+    document.getElementById("generated").innerHTML = generated_con;
+
+    document.getElementById("old").innerHTML += generated_con + ", ";
+}
+
+function generate_spam(amt) {
+    var i = 0;
+    do {
+        generate();
+        i++;
+    } while (i != amt)
+}
+
+function firstload() {
+    document.getElementById("amount-can").innerHTML = "(" + (firstlevel.length * secondlevel.length) + " possibilites)";
+}
